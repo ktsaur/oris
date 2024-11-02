bc908676114db4ef40344146a8530b7eb74689fb
ktsaur <taryshkinakv@gmail.com>
2024-11-02 21:00:42 +0300

    Classwork 02.11

--- src/main/java/ru/kpfu/itis/lesson2/Main.java
+++ src/main/java/ru/kpfu/itis/lesson2/Main.java
@@ -7,79 +7,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        //get
-        try {
-            HttpClientImpl client = new HttpClientImpl();
-            String url = "https://jsonplaceholder.typicode.com/posts?";
-
-            Map<String, String> params = new HashMap<>();
-            params.put("userId", "2");
-            params.put("id", "11");
-
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/json");
-
-            System.out.println(client.get(url, params, headers));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        //post
-        try {
-            HttpClientImpl client = new HttpClientImpl();
-            String url = "https://jsonplaceholder.typicode.com/posts";
-
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/json");
-            headers.put("Accept", "application/json");
-            headers.put("Authorization", "Bearer 58762cdab4e248c10d165f6bbe89d18a444dff00267b6cfcec49acf9dceb94b7");
-
-            Map<String, String> data = new HashMap<>();
-            data.put("userId", "78");
-            data.put("id", "78");
-            data.put("title", "title");
-            data.put("body", "body");
-
-            System.out.println(client.post(url, headers, data));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        //put в пут ошибка пока потом надо исправить
-        try {
-            HttpClientImpl client = new HttpClientImpl();
-            String url = "https://jsonplaceholder.typicode.com/posts/1";
-
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/json");
-            headers.put("Accept", "application/json");
-            headers.put("Authorization", "Bearer 58762cdab4e248c10d165f6bbe89d18a444dff00267b6cfcec49acf9dceb94b7");
-
-            Map<String, String> data = new HashMap<>();
-//            data.put("userId", "78");
-//            data.put("id", "78");
-            data.put("title", "new title");
- //           data.put("body", "new body");
-
-            System.out.println(client.put(url, headers, data));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try{
-            HttpClientImpl client = new HttpClientImpl();
-            String url = "https://jsonplaceholder.typicode.com/posts/1";
-
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/json");
-
-            Map<String, String> data = new HashMap<>();
-
-            System.out.println(client.delete(url, headers, data));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        HttpClient client = new HttpClientImpl();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Accept", "application/json");
+        Map<String, String> params = new HashMap<>();
+        params.put("userId", "2");
+        params.put("id", "11");
+        Map<String, String> data = new HashMap<>();
+        data.put("title", "new title");
+        System.out.println(client.get("http://localhost:8080/classwork_oris_war_exploded/", params, headers));
+        System.out.println(client.post("http://localhost:8080/classwork_oris_war_exploded/", headers, params));
+        System.out.println(client.put("https://jsonplaceholder.typicode.com/posts/1", headers, data));
+        System.out.println(client.delete("https://jsonplaceholder.typicode.com/posts/1", headers, data));
     }
 }
